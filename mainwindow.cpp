66a7d59a3e88eaa70bf5427359713f6527633dbf
zakpl89@gmail.com <zakpl89@gmail.com>
2013-02-09 19:43:12 +0100

    tmp save

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,3 +12,93 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::initAction()
+{
+    aCouper = new QAction();
+    aColler = new QAction();
+    aCopier = new QAction();
+
+    aConnexion = new QAction();
+    aNouveau = new QAction();
+    aSauver = new QAction();
+
+    aGras = new QAction();
+    aItalique = new QAction();
+    aSouligner = new QAction();
+
+    aAligneLeft = new QAction();
+    aAligneRight = new QAction();
+    aAlgneCenter = new QAction();
+    aJustify = new QAction();
+
+    aListe = new QAction();
+    aTableau = new QAction();
+
+    aCouleur = new QAction();
+    aCouleurFond = new QAction();
+    aTaille = new QAction();
+
+    aLien = new QAction();
+    aImage = new QAction();
+    aVideo = new QAction();
+
+    aHelp = new QAction();
+}
+
+void MainWindow::initIcon()
+{
+    iCouper = new QIcon("ressources/images/couper.png");
+    iColler = new QIcon("ressources/images/coller.png");
+    iCopier = new QIcon("ressources/images/copier.png");
+
+    iConnexion = new QIcon("ressources/images/reseau.png");
+    iNouveau = new QIcon("ressources/images/nouveau.png");
+    isauver = new QIcon("ressources/images/nouveau.png");
+
+    iGras = new QIcon("ressources/images/gras.png");
+    iItalique = new QIcon("ressources/images/italique.png");
+    iSouligner = new QIcon("ressources/images/souligner.png");
+
+    iAligneLeft = new QIcon("ressources/images/alignerGauche.png");
+    iAligneRight = new QIcon("ressources/images/alignerDroite.png");
+    iAlgneCenter = new QIcon("ressources/images/centrer.png");
+    iJustify = new QIcon("ressources/images/justifier.png");
+
+    iListe = new QIcon("ressources/images/liste.png");
+    iTableau = new QIcon("ressources/images/tableau.png");
+
+    iCouleur = new QIcon("ressources/images/couleurText.png");
+    iCouleurFond = new QIcon("ressources/images/couleurFond");
+
+    iLien = new QIcon("ressources/images/lien.png");
+    iImage = new QIcon("ressources/images/image.png");
+    iVideo = new QIcon("ressources/images/video.png");
+
+    iHelp = new QIcon("ressources/images/aide.png");
+}
+
+void MainWindow::initMenu()
+{
+
+}
+
+void MainWindow::initToolbar()
+{
+
+}
+
+void MainWindow::initWindows()
+{
+
+}
+
+void MainWindow::initTextEdit()
+{
+
+}
+
+void MainWindow::initTree()
+{
+
+}
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,13 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QAction>
+#include <QMenu>
+#include <QToolBar>
+#include <QMenuBar>
+#include <QTextEdit>
+#include <QTreeView>
+#include <QIcon>
 
 namespace Ui {
 class MainWindow;
@@ -16,7 +23,87 @@ public:
     ~MainWindow();
     
 private:
+    // --- Functions
+    void initAction();
+    void initIcon();
+    void initMenu();
+    void initToolbar();
+    void initWindows();
+    void initTextEdit();
+    void initTree();
+
+    // --- Attributs
     Ui::MainWindow *ui;
+    QMenuBar* menuBar;
+    QToolBar* toolBar;
+    QTextEdit* textEdit;
+    QTreeView* tree;
+
+    //Actions
+    QAction* aCouper;
+    QAction* aColler;
+    QAction* aCopier;
+
+    QAction* aConnexion;
+    QAction* aNouveau;
+    QAction* aSauver;
+
+    QAction* aGras;
+    QAction* aItalique;
+    QAction* aSouligner;
+
+    QAction* aAligneLeft;
+    QAction* aAligneRight;
+    QAction* aAlgneCenter;
+    QAction* aJustify;
+
+    QAction* aListe;
+    QAction* aTableau;
+
+    QAction* aCouleur;
+    QAction* aCouleurFond;
+    QAction* aTaille;
+
+    QAction* aLien;
+    QAction* aImage;
+    QAction* aVideo;
+
+    QAction* aHelp;
+
+    //Icones
+    QIcon* iCouper;
+    QIcon* iColler;
+    QIcon* iCopier;
+
+    QIcon* iConnexion;
+    QIcon* iNouveau;
+    QIcon* iSauver;
+
+    QIcon* iGras;
+    QIcon* iItalique;
+    QIcon* iSouligner;
+
+    QIcon* iAligneLeft;
+    QIcon* iAligneRight;
+    QIcon* iAlgneCenter;
+    QIcon* iJustify;
+
+    QIcon* iListe;
+    QIcon* iTableau;
+
+    QIcon* iCouleur;
+    QIcon* iCouleurFond;
+
+    QIcon* iLien;
+    QIcon* iImage;
+    QIcon* iVideo;
+
+    QIcon* iHelp;
+
+    //Menu
+    QMenu* mFichier;
+    QMenu* mEdition;
+    QMenu* mAide;
 };
 
 #endif // MAINWINDOW_H
